1ebe5c947caf8353d9a7f231bf1061d5e6caa5e6
svidhi08 <svidhi0811@gmail.com>
2026-03-03 19:58:20 +0530

    Fix: Bind Flask app to 0.0.0.0 and use Render dynamic PORT

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pickle
 from flask import Flask, render_template, request
 import re
-
+import os
 app = Flask(__name__)
 
 # -----------------------------
@@ -123,7 +123,8 @@ def recommend():
     )
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 
 # -----------------------------
